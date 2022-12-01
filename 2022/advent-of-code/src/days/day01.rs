8209f127fef252e29a9fc04307e7d472c7582db6
Jan Hönig <jan.hoenig@fau.de>
2022-12-01 11:41:35 +0100

    Nicer

--- 2022/advent-of-code/src/days/day01.rs
+++ 2022/advent-of-code/src/days/day01.rs
@@ -4,7 +4,7 @@ pub fn run(input: String) {
     let mut data_grouped: Vec<u32> = Vec::new();
     for (key, group) in &input.lines().group_by(|l| *l != "") {
         if key{
-            data_grouped.push(group.fold(0u32, |sum, val| return sum + val.parse::<u32>().unwrap()));
+            data_grouped.push(group.flat_map(|c| c.parse::<u32>()).sum());
         }
     }
     println!("Part1: {:?}", data_grouped.iter().max());
