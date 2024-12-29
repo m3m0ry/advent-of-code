cfd58956e88d43a99d9d4042058abfbe9d29fdcc
Jan Hönig <jan.hoenig@fau.de>
2024-12-29 21:35:25 +0100

    Done day 15 part 2

--- 2024/aoc2024/day15.py
+++ 2024/aoc2024/day15.py
@@ -3,6 +3,11 @@ from boiler import AbstractDay
 import numpy as np
 
 
+class Unmovable(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
 def first_space(grid, pos, dir):
     x, y = pos
     match dir:
@@ -52,6 +57,19 @@ def mov_robot(pos, dir):
         case '^':
             return x-1, y
 
+
+def opposit_dir(dir):
+    match dir:
+        case '<':
+            return '>'
+        case '>':
+            return '<'
+        case 'v':
+            return '^'
+        case '^':
+            return 'v'
+
+
 class Day15(AbstractDay):
     def preprocess(self):
         for i, l in enumerate(self.puzzle):
@@ -67,7 +85,7 @@ class Day15(AbstractDay):
 
 
     def part1(self):
-        grid = self.grid
+        grid = self.grid.copy()
         mov = self.movements
         pos = self.robot
         for m in mov:
@@ -99,5 +117,83 @@ class Day15(AbstractDay):
         return res
 
     def part2(self):
-        return None
+        mov = self.movements
 
+        shape = self.grid.shape
+        grid = np.full((shape[0], shape[1]*2), '.')
+        grid[:, ::2] = self.grid
+        boxes = np.argwhere(grid == 'O')
+        for i,j in boxes:
+            grid[i,j] = '['
+            grid[i,j+1] = ']'
+
+        shape = grid.shape
+        walls = np.argwhere(grid == '#')
+        for i,j in walls:
+            grid[i,j+1] = '#'
+
+
+        pos = (self.robot[0], self.robot[1]*2)
+
+        grid[*pos] = '@'
+        for m in mov:
+            if m in '<>':
+                if (space := first_space(grid, pos, m)):
+                    k, l = space
+                    x, y = pos
+                    if x == k:
+                        if y > l:
+                            grid[x, l:y] = grid[x, l+1:y+1]
+                            grid[*pos] = '.'
+                        else:
+                            grid[x, y+1:l+1] = grid[x, y:l]
+                            grid[*pos] = '.'
+                    elif l == y:
+                        if x > k:
+                            grid[k:x, l] = grid[k+1:x+1, l]
+                            grid[*pos] = '.'
+                        else:
+                            grid[x+1:k+1, l] = grid[x:k, l]
+                            grid[*pos] = '.'
+
+                    pos = mov_robot(pos, m)
+            else: # 'v^'
+                try:
+                    to_move = find_boxes(grid, mov_robot(pos, m), m)
+                    to_move.add(pos)
+                    old = grid.copy()
+                    for move in to_move:
+                        grid[*move] = '.'
+                    for move in to_move:
+                        grid[*mov_robot(move, m)] = old[*move]
+                    grid[*pos] = '.'
+
+                    pos = mov_robot(pos, m)
+
+                except Unmovable as _:
+                    pass
+
+        res = 0
+        crates = np.argwhere(grid == '[')
+        for crate in crates:
+            res += 100 * crate[0] + crate[1]
+
+        return res
+
+def find_boxes(grid, pos, m):
+    if grid[*pos] == '[':
+        side = mov_robot(pos, '>')
+        box = {pos, side}
+        box |= find_boxes(grid, mov_robot(pos, m), m)
+        box |= find_boxes(grid, mov_robot(side, m), m)
+        return box
+    elif grid[*pos] == ']':
+        side = mov_robot(pos, '<')
+        box = {pos, side}
+        box |= find_boxes(grid, mov_robot(pos, m), m)
+        box |= find_boxes(grid, mov_robot(side, m), m)
+        return box
+    elif grid[*pos] == '.':
+        return set()
+    else:
+        raise Unmovable('No space to push into')
